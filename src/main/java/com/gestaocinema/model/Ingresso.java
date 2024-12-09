6b32d7cbe463cd77d69674da769f9c6ed8317b61
Arthur <arthur012002@gmail.com>
2024-12-09 01:39:02 -0300

    Testando .gitignore

--- src/main/java/com/gestaocinema/model/Ingresso.java
+++ src/main/java/com/gestaocinema/model/Ingresso.java
@@ -1,7 +1,35 @@
 package com.gestaocinema.model;
 
 public class Ingresso {
+    /*private int id;
+    private Sessao sessao;
+    private int cadeira;
+    private double preco;
 
-	
-	
+    public Ingresso(int id, Sessao sessao, int cadeira, double preco) {
+        if (sessao.isCadeiraOcupada(cadeira)) {
+            throw new IllegalArgumentException("Cadeira já ocupada!");
+        }
+        this.id = id;
+        this.sessao = sessao;
+        this.cadeira = cadeira;
+        this.preco = preco;
+        sessao.ocuparCadeira(cadeira);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Sessao getSessao() {
+        return sessao;
+    }
+
+    public int getCadeira() {
+        return cadeira;
+    }
+
+    public double getPreco() {
+        return preco;
+    }*/
 }
